a974c461140e1c7aea0b11e63fcf7ae32522c22e
Luiz Gustavo Rocco <tgv.lrocco@ifood3rd.com>
2025-10-15 17:20:02 -0300

    Creating the Scanner class

---
+++ Scanner.ts
@@ -0,0 +1,146 @@
+import { Lox } from "./Lox.ts";
+import { LoxLiteral, Token } from "./Token.ts";
+import { TokenType } from "./TokenType.ts";
+
+export class Scanner {
+  private readonly source: string;
+  private readonly tokens: Token[] = [];
+  private start = 0;
+  private current = 0;
+  private line = 1;
+
+  constructor(source: string) {
+    this.source = source;
+  }
+
+  scanTokens() {
+    while (!this.isAtEnd()) {
+      this.start = this.current;
+      this.scanToken();
+    }
+
+    this.tokens.push(new Token(TokenType.EOF, "", null, this.line));
+    return this.tokens;
+  }
+
+  private scanToken(): void {
+    const c = this.advance();
+    switch (c) {
+      case "(":
+        this.addToken(TokenType.LEFT_PAREN);
+        break;
+      case ")":
+        this.addToken(TokenType.RIGHT_PAREN);
+        break;
+      case "{":
+        this.addToken(TokenType.LEFT_BRACE);
+        break;
+      case "}":
+        this.addToken(TokenType.RIGHT_BRACE);
+        break;
+      case ",":
+        this.addToken(TokenType.COMMA);
+        break;
+      case ".":
+        this.addToken(TokenType.DOT);
+        break;
+      case "-":
+        this.addToken(TokenType.MINUS);
+        break;
+      case "+":
+        this.addToken(TokenType.PLUS);
+        break;
+      case ";":
+        this.addToken(TokenType.SEMICOLON);
+        break;
+      case "*":
+        this.addToken(TokenType.STAR);
+        break;
+      case "!":
+        this.addToken(this.match("=") ? TokenType.BANG_EQUAL : TokenType.BANG);
+        break;
+      case "=":
+        this.addToken(
+          this.match("=") ? TokenType.EQUAL_EQUAL : TokenType.EQUAL
+        );
+        break;
+      case "<":
+        this.addToken(this.match("=") ? TokenType.LESS_EQUAL : TokenType.LESS);
+        break;
+      case ">":
+        this.addToken(
+          this.match("=") ? TokenType.GREATER_EQUAL : TokenType.GREATER
+        );
+        break;
+      case "/":
+        if (this.match("/")) {
+          // A comment goes until the end of the line without consuming the new line character.
+          while (this.peek() != "\n" && !this.isAtEnd()) this.advance();
+        } else {
+          this.addToken(TokenType.SLASH);
+        }
+        break;
+      case " ":
+      case "\r":
+      case "\t":
+        // Ignore whitespace.
+        break;
+      case "\n":
+        this.line++;
+        break;
+      case '"':
+        this.string();
+        break;
+      default:
+        Lox.error(this.line, "Unexpected character.");
+        break;
+    }
+  }
+
+  private string(): void {
+    while (this.peek() != '"' && !this.isAtEnd()) {
+      if (this.peek() == "\n") this.line++;
+      this.advance();
+    }
+
+    if (this.isAtEnd()) {
+      Lox.error(this.line, "Unterminated string.");
+      return;
+    }
+
+    // The closing ".
+    this.advance();
+
+    // Trim the surrounding quotes.
+    const value = this.source.slice(this.start + 1, this.current - 1);
+    this.addToken(TokenType.STRING, value);
+  }
+
+  private match(expected: string): boolean {
+    if (this.isAtEnd()) return false;
+    if (this.source.charAt(this.current) != expected) return false;
+
+    this.current++;
+    return true;
+  }
+
+  private peek(): string {
+    if (this.isAtEnd()) return "\0";
+    return this.source.charAt(this.current);
+  }
+
+  private isAtEnd(): boolean {
+    return this.current >= this.source.length;
+  }
+
+  private advance(): string {
+    return this.source.charAt(this.current++);
+  }
+
+  private addToken(type: TokenType): void;
+  private addToken(type: TokenType, literal: LoxLiteral): void;
+  private addToken(type: TokenType, literal?: LoxLiteral): void {
+    const text = this.source.slice(this.start, this.current);
+    this.tokens.push(new Token(type, text, literal ?? null, this.line));
+  }
+}
