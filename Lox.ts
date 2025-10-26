840a5faaadb316e30bce59c168740772846ffb53
Luiz Gustavo Rocco <tgv.lrocco@ifood3rd.com>
2025-10-26 15:33:34 -0300

    Refactor lox class to use the interpreter and parser

--- Lox.ts
+++ Lox.ts
@@ -1,16 +1,25 @@
+import { Interpreter } from "./Interpreter.ts";
+import { Parser } from "./Parser.ts";
+import { RuntimeError } from "./RuntimeError.ts";
 import { Scanner } from "./Scanner.ts";
+import { Token } from "./Token.ts";
+import { TokenType } from "./TokenType.ts";
 
 export class Lox {
+  private static readonly interpreter = new Interpreter();
   static hadError = false;
+  static hadRuntimeError = false;
 
-  main(args: string[]) {
+  static main(args: string[]) {
+    const lox = new Lox();
+
     if (args.length > 1) {
       console.log("Usage: deno run lox.ts [script]");
       Deno.exit(64);
     } else if (args.length === 1) {
-      this.runFile(args[0]);
+      lox.runFile(args[0]);
     } else {
-      this.runPrompt();
+      lox.runPrompt();
     }
   }
 
@@ -18,6 +27,7 @@ export class Lox {
     const source = Deno.readTextFileSync(path);
     this.run(source);
     if (Lox.hadError) Deno.exit(65);
+    if (Lox.hadRuntimeError) Deno.exit(70);
   }
 
   private runPrompt() {
@@ -32,12 +42,31 @@ export class Lox {
   private run(source: string) {
     const scanner = new Scanner(source);
     const tokens = scanner.scanTokens();
-    // For now, just print the tokens.
-    console.log(tokens);
+    const parser = new Parser(tokens);
+    const expression = parser.parse();
+
+    if (Lox.hadError || expression === null) return;
+
+    Lox.interpreter.interpret(expression);
   }
 
-  static error(line: number, message: string) {
-    this.report(line, "", message);
+  static error(line: number, message: string): void;
+  static error(token: Token, message: string): void;
+  static error(lineOrToken: number | Token, message: string): void {
+    if (typeof lineOrToken === "number") {
+      const line = lineOrToken;
+      this.report(line, "", message);
+    } else {
+      const token = lineOrToken;
+      const where =
+        token.type === TokenType.EOF ? " at end" : ` at '${token.lexeme}'`;
+      this.report(token.line, where, message);
+    }
+  }
+
+  static runtimeError(error: RuntimeError): void {
+    console.log(error.message + "\n[line " + error.token.line + "]");
+    this.hadRuntimeError = true;
   }
 
   private static report(line: number, where: string, message: string) {
@@ -47,6 +76,5 @@ export class Lox {
 }
 
 if (import.meta.main) {
-  const lox = new Lox();
-  lox.main(Deno.args);
+  Lox.main(Deno.args);
 }
